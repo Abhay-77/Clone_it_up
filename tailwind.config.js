69ee3fdb607da7721ab037e6cc549ca933b96b9b
Abhay-77 <abhaymuralim9@gmail.com>
2024-09-18 02:31:05 +0530

    font colour

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,18 @@ module.exports = {
   theme: {
     extend: {
       colors:{
-        nSilver : "#F5F7FA"
-      }
+        nSilver : "#F5F7FA",
+        nBlack : "#263238",
+        nD_Grey : "#4D4D4D",
+        nGrey : "#717171",
+        nL_Grey : "#89939E",
+        nGrey_Blue : "#ABBED1",
+        nWhite : "#FFFFFF",
+        primary : "#28CB8B",
+        secondary : "263238",
+        info : "2194F3"
+      },
+      
     },
   },
   plugins: [],
